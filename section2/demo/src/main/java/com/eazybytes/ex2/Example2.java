d1b8f65872f8f0349907ed49e859794adc17c134
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-03-13 18:58:22 +0530

    Creating Custom Beans

---
+++ section2/demo/src/main/java/com/eazybytes/ex2/Example2.java
@@ -0,0 +1,19 @@
+package com.eazybytes.ex2;
+
+import com.eazybytes.ex2.beans.Vehicle;
+import com.eazybytes.ex2.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example2{
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);//this is used for starting the spring container
+
+        var veh = context.getBean("audiVehicle", Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        var vehicle = (Vehicle) context.getBean("ferrariVehicle");
+        System.out.println("Vehicle name from Spring Context is: " + vehicle.getName());
+    }
+}
