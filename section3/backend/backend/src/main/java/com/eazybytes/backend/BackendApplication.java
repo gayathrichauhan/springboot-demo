f87726540fab8ede98e03699c25641b51921ca17
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-03-18 15:27:21 +0530

    use of @SpringBootApplication

--- section3/backend/backend/src/main/java/com/eazybytes/backend/BackendApplication.java
+++ section3/backend/backend/src/main/java/com/eazybytes/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.eazybytes.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages ="com.eazybytes.outside.controller" )
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- section3/backend/backend/src/main/java/com/eazybytes/controller/DemoController.java
+++ section3/backend/backend/src/main/java/com/eazybytes/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.eazybytes.backend.controller;
+package com.eazybytes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
