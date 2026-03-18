b10c25ba21ea5896afb9c50c15aa636780a10187
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-03-18 22:03:23 +0530

    autoconfiguration in springboot

--- section3/backend/backend/src/main/java/com/eazybytes/backend/BackendApplication.java
+++ section3/backend/backend/src/main/java/com/eazybytes/backend/BackendApplication.java
@@ -2,10 +2,11 @@ package com.eazybytes.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan(basePackages ="com.eazybytes.outside.controller" )
+@SpringBootApplication//(exclude={ErrorMvcAutoConfiguration.class})
+//@ComponentScan(basePackages ="com.eazybytes.controller" )
 public class BackendApplication {
 
 	public static void main(String[] args) {
