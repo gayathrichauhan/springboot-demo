7a6905be5d63f29ba847be5fb7ae2220a6bc426e
gayathrichauhan <gayathrichauhan090@gmail.com>
2026-03-20 23:54:41 +0530

    use of @RequestMapping

--- section3/backend/backend/src/main/java/com/eazybytes/backend/UserController.java
+++ section3/backend/backend/src/main/java/com/eazybytes/backend/UserController.java
@@ -2,14 +2,16 @@ package com.eazybytes.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/dummy/users")
 public class UserController {
 
-    @GetMapping("/api/dummy/users/{userId}/address/{addressId}")
+    @GetMapping("/{userId}/address/{addressId}")
     public String searchUserPostWithMultiPathVariable(
             @PathVariable Map<String, String> pathVariablesMap) {
 
@@ -17,5 +19,9 @@ public class UserController {
                 + pathVariablesMap.get("userId")
                 + " and address id: "
                 + pathVariablesMap.get("addressId");
+    }
+    @GetMapping("/search")
+    public String searchUserWithQueryparams(){
+        return "fetched user with query params";
     }
 }
